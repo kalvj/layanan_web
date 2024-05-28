c48213e237b005c60572eaa0bbaacaeb08e73230
Adirahman Haikal <adir57545@gmail.com>
2024-05-28 20:31:08 +0700

    menambahkan login page

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        // Proses login, validasi, dan autentikasi bisa ditambahkan di sini
+        // Untuk saat ini kita hanya akan redirect ke home setelah login
+        return redirect()->route('home');
+    }
+}
